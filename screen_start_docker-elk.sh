83ba20a12e1b59cae08d25e8e6c13250cfcc3c8b
Ari Braginsky <contact@aribraginsky.com>
2019-04-27 19:18:44 -0700

    - Created launchd config so that docker-elk starts after rebooting.

---
+++ screen_start_docker-elk.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+screen -DmS docker-elk /Users/psicom/Source/_raspberry_pi/local/local/docker/source/docker-elk/start_docker-elk.sh &
---
+++ start_docker-elk.sh
@@ -0,0 +1,2 @@
+#!/bin/basin
+cd /Users/psicom/Source/_raspberry_pi/local/local/docker/source/docker-elk && docker-compose up
